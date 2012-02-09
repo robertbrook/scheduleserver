054f60b88df8a11103eb948ad8f813e0a83fec57
lizconlan <lizconlan@gmail.com>
2012-02-09 14:07:05 +0000

    added date scope to queries - only current and future items being returned

--- server.rb
+++ server.rb
@@ -33,32 +33,32 @@ end
 get "/:house.ics" do
   #content_type 'text/calendar'
   house = params[:house]
-  @items = Item.find_all_by_house(house, :order => "date DESC, start_time")
+  @items = Item.find_all_by_house(house, :date => { "$gte" => Time.now.strftime("%Y-%m-%d") }, :order => "date DESC, start_time")
 end
 
 get "/commons_main_chamber.ics" do
   #content_type 'text/calendar'
-  @items = Item.find_all_by_house_and_location("Commons", "Main Chamber", :order => "date DESC, start_time")
+  @items = Item.find_all_by_house_and_location("Commons", "Main Chamber", :date => { "$gte" => Time.now.strftime("%Y-%m-%d") }, :order => "date DESC, start_time")
 end
 
 get "/commons_westminster_hall.ics" do
   #content_type 'text/calendar'
-  @items = Item.find_all_by_house_and_location("Commons", "Westminster Hall", :order => "date DESC, start_time")
+  @items = Item.find_all_by_house_and_location("Commons", "Westminster Hall", :date => { "$gte" => Time.now.strftime("%Y-%m-%d") }, :order => "date DESC, start_time")
 end
 
 get "/commons_general_committee.ics" do
   #content_type 'text/calendar'
-  @items = Item.find_all_by_house_and_sponsor("Commons", /^General Committee/, :order => "date DESC, start_time")
+  @items = Item.find_all_by_house_and_sponsor("Commons", /^General Committee/, :date => { "$gte" => Time.now.strftime("%Y-%m-%d") }, :order => "date DESC, start_time")
 end
 
 get "/commons_select_committee.ics" do
   #content_type 'text/calendar'
-  @items = Item.find_all_by_house_and_sponsor("Commons", /^Select Committee/, :order => "date DESC, start_time")
+  @items = Item.find_all_by_house_and_sponsor("Commons", /^Select Committee/, :date => { "$gte" => Time.now.strftime("%Y-%m-%d") }, :order => "date DESC, start_time")
 end
 
 get "/lords_main_chamber.ics" do
   #content_type 'text/calendar'
-  @items = Item.find_all_by_house_and_location("Lords", "Main Chamber", :order => "date DESC, start_time")
+  @items = Item.find_all_by_house_and_location("Lords", "Main Chamber", :date => { "$gte" => Time.now.strftime("%Y-%m-%d") }, :order => "date DESC, start_time")
 end
 
 get "/lords_grand_committee.ics" do
